88788cf1c10abeea6b700033cd31b6675e9b2c0b
fredericpernaton <166522829+fredericpernaton@users.noreply.github.com>
2024-04-30 16:57:11 +0200

    musique

---
+++ script.js
@@ -0,0 +1,39 @@
+fetch("instrumentsMusiques.json")
+.then(rep=>{
+    return rep.json()
+})
+.then(data=>{
+    console.log(data.entreprise.produits)
+    affichelesInstruments(data.entreprise.produits)
+    affichelesServices(data.entreprise.Services)
+})
+
+function affichelesInstruments(instruments){
+    // instruments : le tableau de produits
+    // pour chacun des instruments du tableau : 
+    instruments.array.forEach(instru => {
+        
+    
+       // construire une petite carte
+       // l'ajouter dans la div qui a l'id instruments
+       document.querySelector("#instrument").innerHTML +=`<div class="card">
+       <img src="${instru.image}" alt="">
+       <h4>${instru.nom}</h4>
+       <p>${instru.description}</p>
+   </div>
+   `
+    });
+}
+
+  function affichelesServices(Services){
+    Services.forEach(s=>{
+        console.log(s)
+        document.querySelector("#Services").innerHTML+=` <div class="carte-service">
+        <img src="" alt="">
+        <h4>Nom du service</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero a dolore nulla? Dolore repellat, enim quod totam sit tenetur, iure officiis exercitationem dolores, velit distinctio officia illum suscipit. Accusantium, a?</p>
+        <a href="" class="bouton"></a>
+    </div>`
+    })
+  }
+
